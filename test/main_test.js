c229764f8f8978eb52e7f02783838e3ee054647b
pjo901018 <pjo901018@gmail.com>
2017-08-25 05:16:37 +0900

    Making first suite as sanity check.

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('Sanity check', function () {
+    // Test spec (unit test)
+    it('should run our tests using npm', function () {
+        expect(true).to.be.ok;
+    })
+});
