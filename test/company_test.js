f1829afaeb4688a7a2db005a680d62683cf9703d
pjo901018 <pjo901018@korea.ac.kr>
2017-08-27 10:25:05 +0900

    adding Before block

--- test/company_test.js
+++ test/company_test.js
@@ -4,51 +4,61 @@ var expect = chai.expect;
 var app = require("../app");
 chai.use(chaiHttp);
 
+describe('Company CRUD Tests:', function() {
+    describe('GET', function () {
+        var Company;
 
-describe('Company GET', function () {
-
-    it('api/companies/ return 200', function (done) {
-
-        chai.request(app)
-            .get('/api/companies')
-            .end(function (err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
+        before(function (done) {
+            Company = require("../models/models").Company;
+            Company.remove({}, function () {
                 done();
             });
+
+            // Company.
+        });
+
+        it('api/companies/ return 200', function (done) {
+            chai.request(app)
+                .get('/api/companies')
+                .end(function (err, res) {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(200);
+                    done();
+                });
+        });
+
+        Company
     });
 
-});
+    describe('Company POST', function () {
 
-describe('Company POST', function () {
+        it('api/companies/ return 201', function (done) {
+            chai.request(app)
+                .post('/api/companies')
+                .send({
+                    'company_name': 'testCompany2',
+                })
+                .end(function (err, res) {
+                    expect(res).to.have.status(201);
+                    done();
+                });
+        });
 
-    it('POST api/companies/ return 201', function (done) {
+        it('api/companies/ return same body as I sended', function (done) {
 
-        chai.request(app)
-            .post('/api/companies')
-            .send({
-                'company_name': 'testCompany2',
-            })
-            .end(function (err, res) {
-                expect(res).to.have.status(201);
-                done();
-            });
-    });
-
-    it('POST api/companies/ return same body as I sended', function (done) {
-
-        chai.request(app)
-            .post('/api/companies')
-            .send({
-                'company_name': 'testCompany3',
-            })
-            .end(function (err, res) {
-                expect(res.body).to.equal(
-                    {
-                        'company_name': 'testCompany'
-                    }
-                );
-                done();
-            });
+            chai.request(app)
+                .post('/api/companies')
+                .send({
+                    'company_name': 'testCompany3',
+                })
+                .end(function (err, res) {
+                    expect(res.body).to.equal(
+                        {
+                            'company_name': 'testCompany'
+                        }
+                    );
+                    done();
+                });
+        });
     });
 });
